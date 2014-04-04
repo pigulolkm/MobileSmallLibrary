8d03cc415fa073c4bd4039008368d987c7b5c226
pigulolkm <pigulolkm@gmail.com>
2014-04-04 23:25:33 +0800

    Create setting activity

--- src/com/example/mobilesmalllibrary/MainActivity.java
+++ src/com/example/mobilesmalllibrary/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -139,5 +140,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    
+	    switch(item.getItemId())
+	    {
+	    	// setting
+	    	case R.id.action_settings:
+	    		Intent intent = new Intent();
+	    		intent.setClass(MainActivity.this, SettingsActivity.class);
+	    		startActivity(intent);
+	    		return true;
+	    	default :
+	    		return false;
+	    }
 	}
 }
