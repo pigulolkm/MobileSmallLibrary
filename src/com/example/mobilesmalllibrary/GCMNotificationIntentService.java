4b0662ef73c34536281e9fce466b12e8dc4378a2
pigulolkm <pigulolkm@gmail.com>
2014-04-14 15:35:47 +0800

    Finish notification see desc
    1. Notification with light, sound, vibration in default
    2. Click on the notification will not start a new activity
    if the activity is in the runnging task
    but resume it and intent to mainActivity

--- src/com/example/mobilesmalllibrary/GCMNotificationIntentService.java
+++ src/com/example/mobilesmalllibrary/GCMNotificationIntentService.java
@@ -1,10 +1,12 @@
 package com.example.mobilesmalllibrary;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
@@ -16,7 +18,7 @@ public class GCMNotificationIntentService extends IntentService {
 
 	public static final int NOTIFICATION_ID = 1;
 	private NotificationManager mNotificationManager;
-	NotificationCompat.Builder builder;
+	Notification notification;
 	
 	private static final String TAG = "GCMNotificationIntentService";
 	
@@ -55,7 +57,7 @@ public class GCMNotificationIntentService extends IntentService {
 				}
 				Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
 				
-				sendNotification("Message Received from Google GCM Server: " + extras.get(Generic.MESSAGE_KEY));
+				sendNotification(extras.get(Generic.MESSAGE_KEY).toString());
 				Log.i(TAG, "Received: " + extras.toString());
 			}
 		}
@@ -67,16 +69,22 @@ public class GCMNotificationIntentService extends IntentService {
 		Log.d(TAG, "Preparing to send notification... : " + msg);
 		mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 		
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+		Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);// set intent so it does not start a new activity
 		
-		builder = new NotificationCompat.Builder(this)
-					.setSmallIcon(R.drawable.launcher)
-					.setContentTitle("Mobile Small Library")
-					.setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-					.setContentText(msg);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT); // set PendingIntent flag that is does not start a new activity
 		
-		builder.setContentIntent(contentIntent);
-		mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+		notification = new NotificationCompat.Builder(this)
+						.setSmallIcon(R.drawable.launcher)
+						.setContentTitle("Mobile Small Library")
+						.setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+						.setContentText(msg)
+						.setDefaults(Notification.DEFAULT_ALL)
+						.setAutoCancel(true)
+						.setContentIntent(contentIntent)
+						.build();
+		
+		mNotificationManager.notify(NOTIFICATION_ID, notification);
 		Log.d(TAG, "Notification sent sucessfully");
 	}
 }
